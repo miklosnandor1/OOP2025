761e21d86254f778432e0b5f190982bb5177e078
miklosnandor1 <79194298+miklosnandor1@users.noreply.github.com>
2025-03-04 19:00:45 +0200

    Update Main.java
    hf2

--- labor-02/src/oop/labor02/Main.java
+++ labor-02/src/oop/labor02/Main.java
@@ -1,7 +1,25 @@
 package oop.labor02;
+import java.util.Random;
+import oop.labor02.rectangle.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Lab_02");
+        Random rand = new Random();
+        Rectangle[] rectangles = new Rectangle[10];
+
+        for (int i = 0; i < rectangles.length; i++) {
+            double length = 1 + rand.nextInt(10);
+            double width = 1 + rand.nextInt(10);
+            rectangles[i] = new Rectangle(length, width);
+        }
+
+        double totalArea = 0;
+        for (Rectangle rect : rectangles) {
+            System.out.println(rect);
+            totalArea += rect.area();
+        }
+
+        System.out.println("Total area of all rectangles: " + totalArea);
     }
 }
+
